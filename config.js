2ce154da537d597628d62cb926dda06b6038a10f
mvlimber <marc.van_limberghen@mail.be>
2021-12-01 21:55:48 +0100

    1 file modified, 1 added

---
+++ config.js
@@ -0,0 +1,11 @@
+<script>
+    var firebaseConfig = {
+        apiKey: "AIzaSyDVT644vSE0ue8KyDy-o9YwbYPX3m0ZmSc",
+        authDomain: "slimmeeigendomdb.firebaseapp.com",
+        projectId: "slimmeeigendomdb",
+        storageBucket: "slimmeeigendomdb.appspot.com",
+        messagingSenderId: "636221237434",
+        appId: "1:636221237434:web:3db507acbf4a6a1e0d7a83",
+        measurementId: "G-2JL08QYJJ3"
+      };
+</script>
