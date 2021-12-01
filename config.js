f294e70a93396c9b62e1e91a592cada99077d485
mvlimber <marc.van_limberghen@mail.be>
2021-12-01 22:45:34 +0100

    2 files modified

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
-<script>
-    var firebaseConfig = {
-        apiKey: "AIzaSyDVT644vSE0ue8KyDy-o9YwbYPX3m0ZmSc",
+
+    export default function getConfig() { return
+      {
+      apiKey: "AIzaSyDVT644vSE0ue8KyDy-o9YwbYPX3m0ZmSc",
         authDomain: "slimmeeigendomdb.firebaseapp.com",
         projectId: "slimmeeigendomdb",
         storageBucket: "slimmeeigendomdb.appspot.com",
@@ -8,4 +9,4 @@
         appId: "1:636221237434:web:3db507acbf4a6a1e0d7a83",
         measurementId: "G-2JL08QYJJ3"
       };
-</script>
+    };
